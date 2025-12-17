f4b066f0710139ef9098d069f58a86a6d17ef942
Arilson <arilsonalbano625@gmail.com>
2025-12-17 19:27:25 +0100

    make, parse, philo.c, header

---
+++ Makefile
@@ -0,0 +1,18 @@
+NAME = philo
+CFLAGS = -Wall -Wextra -Werror
+CC = cc
+
+SRC = parse.c \
+
+OBJ = $(SRC:.c=.o)
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	$(CC) $(CFLAGS) $(OBJ) -o $(NAME)
+
+clean:
+	rm -f $(OBJ)
+fclean: clean
+	rm -f $(NAME)
+re: fclean all
---
+++ philo.h
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalbano <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/11 17:24:11 by aalbano           #+#    #+#             */
+/*   Updated: 2025/12/11 17:24:12 by aalbano          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef PHILO_H
+#define PHILO_H
+
+# include <stdio.h>
+# include <sys/time.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <pthread.h>
+
+typedef struct s_rules
+{
+    int         n_philo;
+    long    time_eat;
+    long    time_sleep;
+    long    time_died;
+    int         must_eat;
+    long    start_time;
+    int     someone_died;
+    pthread_mutex_t *forks;
+    pthread_mutex_t print;
+}   t_rules;
+
+typedef struct s_philo
+{
+    int         id;
+    int         meal_eaten;
+    long    last_meal;
+    pthread_t   thread;
+    pthread_mutex_t *left_fork;
+    pthread_mutex_t *rigth_fork;
+    t_rules *rules;
+
+}   t_philo;
+
+//Funcoes do parse
+int     is_positive_number(char *s);
+long    ft_atol(char *s);
+int parse_args(int ac, char **av, t_rules *rules);
+#endif
